7a2e2e90f4c50c229c593ac2b6efde5521a87f3c
ErikBoesen <me@erikboesen.com>
2020-06-11 03:44:12 -0400

    Add initial code from tic tac toe

---
+++ bot.py
@@ -0,0 +1,134 @@
+# Flask
+from flask import Flask, request
+
+# Other
+import mebots
+from threading import Thread
+import requests
+import os
+import time
+from game import Game
+
+
+app = Flask(__name__)
+bot = mebots.Bot("tictactoebot", os.environ.get("BOT_TOKEN"))
+
+MAX_MESSAGE_LENGTH = 1000
+PREFIX = "/"
+
+games = {}
+
+
+# Webhook receipt and response
+@app.route("/", methods=["POST"])
+def receive():
+    """
+    Receive callback to URL when message is sent in the group.
+    """
+    # Retrieve data on that single GroupMe message.
+    message = request.get_json()
+    group_id = message["group_id"]
+    # Begin reply process in a new thread.
+    # This way, the request won't time out if a response takes too long to generate.
+    Thread(target=reply, args=(message, group_id)).start()
+    return "ok", 200
+
+
+def reply(message, group_id):
+    send(process_message(message), group_id)
+
+
+def process_message(message):
+    if message["sender_type"] == "user":
+        if message["text"].startswith(PREFIX):
+            query = message["text"][len(PREFIX):].strip()
+            arguments = query.split()
+            command = arguments.pop(0).lower()
+            group_id = message["group_id"]
+            game = games.get(group_id)
+            user_id = message["user_id"]
+
+            if command in ("start", "join"):
+                if game is None:
+                    game = games[group_id] = Game()
+                    game.join(message["name"], user_id)
+                    return game.players[0].name + " has joined, waiting on a second player. Say #join to join!"
+                elif not game.is_full:
+                    if not game.join(message["name"], user_id):
+                        return "Already in game!"
+                    return [
+                        game.players[1].name + " has joined, starting game!",
+                        game.log_turn()
+                    ]
+                else:
+                    return f"Game full. {game.players[0].name} & {game.players[1].name} are playing!"
+            elif command == "end":
+                games.pop(group_id)
+                return "Game ended."
+            elif game.is_valid_position(command):
+                if not game.in_turn(user_id):
+                    return "Not your turn!"
+                index = game.get_index(command)
+                if game.is_occupied(index):
+                    return "That spot is taken."
+                game.take_turn(index)
+                if game.tied():
+                    response = game.log_tie()
+                    games.pop(group_id)
+                    return response
+                winner = game.winner()
+                if winner is not None:
+                    response = game.log_win(winner)
+                    games.pop(group_id)
+                    return response
+                return game.log_turn()
+            else:
+                return "Unknown command."
+
+
+def send(message, group_id):
+    """
+    Reply in chat.
+    :param message: text of message to send. May be a tuple with further data, or a list of messages.
+    :param group_id: ID of group in which to send message.
+    """
+    if message is None:
+        return
+    # Recurse when sending multiple messages.
+    if isinstance(message, list):
+        for item in message:
+            send(item, group_id)
+        return
+    data = {
+        "bot_id": bot.instance(group_id).id,
+    }
+    if len(message) > MAX_MESSAGE_LENGTH:
+        # If text is too long for one message, split it up over several
+        for block in [message[i:i + MAX_MESSAGE_LENGTH] for i in range(0, len(message), MAX_MESSAGE_LENGTH)]:
+            send(block, group_id)
+            time.sleep(0.3)
+        data["text"] = ""
+    else:
+        data["text"] = message
+    # Prevent sending message if there's no content
+    # It would be rejected anyway
+    response = requests.post("https://api.groupme.com/v3/bots/post", data=data)
+
+
+# Local testing
+if __name__ == "__main__":
+    message = {"sender_type": "user", "group_id": 49940116}
+    current_user = 0
+    while True:
+        text = input("Player %d > " % (current_user + 1))
+        if not text:
+            current_user = not current_user
+        else:
+            message["user_id"] = int(current_user)
+            message["name"] = "Player %d" % (current_user + 1)
+            message["text"] = text
+            response = process_message(message)
+            if isinstance(response, list):
+                print('\n'.join(response))
+            else:
+                print(response)
---
+++ game.py
@@ -0,0 +1,89 @@
+class Player:
+    def __init__(self, name, user_id):
+        self.name = name
+        self.user_id = user_id
+
+class Game:
+    PIECES = ["X", "O"]
+
+    def __init__(self):
+        self.players = []
+        self.board = [" "] * 9
+        self.turn = 0
+
+    @property
+    def is_full(self):
+        return len(self.players) == 2
+
+    def join(self, name, user_id):
+        for player in self.players:
+            if player.user_id == user_id:
+                return False
+        self.players.append(Player(name, user_id))
+        return True
+
+    # Methods for executing a turn
+
+    def is_valid_position(self, position: str):
+        return len(position) == 2 and position[0] in "abcdefgh" and position[1] in "12345678"
+
+    def get_index(self, position: str):
+        return ((int(position[1]) - 1) * 3 + (ord(position[0]) - ord('a')))
+
+    def is_occupied(self, index: int) -> bool:
+        return not self.board[index] == " "
+
+    def in_turn(self, user_id):
+        return self.players[self.turn].user_id == user_id
+
+    def take_turn(self, index: int):
+        self.board[index] = self.PIECES[self.turn]
+        self.turn = not self.turn
+
+    # Methods for outputting information in string form
+
+    def safe_spaces(self, text):
+        return text.replace(" ", "\u2004")
+
+    def log_board(self):
+        with open("board.txt") as f:
+            board = f.read()
+        board = board % tuple(self.board)
+        return self.safe_spaces(board)
+
+    def log_turn(self):
+        return (
+            self.log_board() + '\n\n' +
+            ("It is %s's turn (%s). To take a turn, say # followed by the position for the square to play in, like A1." % (self.PIECES[self.turn], self.players[self.turn].name))
+        )
+
+    def log_win(self, winner: int):
+        return (
+            self.log_board() + '\n\n' +
+            ("🎉 %s (%s) wins! Say #start to play again." % (self.PIECES[winner], self.players[winner].name))
+        )
+
+    def log_tie(self):
+        return (
+            self.log_board() + '\n\n' +
+            "It's a tie! Say #start to play again."
+        )
+
+    # Completion checking
+
+    def tied(self):
+        return all([is_occupied(i) for i in range(0, 9)])
+
+    def winner(self):
+        runs = [
+            slice(0, 3), slice(3, 6), slice(6, 9),
+            slice(0, 9, 3), slice(1, 9, 3), slice(2, 9, 3),
+            # Diagonals
+            slice(0, 9, 4), slice(2, 7, 2)
+        ]
+        for run in runs:
+            if self.board[run] == [self.PIECES[0]] * 3:
+                return 0
+            if self.board[run] == [self.PIECES[1]] * 3:
+                return 1
+        return None
